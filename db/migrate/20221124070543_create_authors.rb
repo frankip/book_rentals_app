1a70adaed4af1c607974858afb6200712605a4b2
Franck <gwadafrank@gmail.com>
2022-11-24 10:10:59 +0300

    Initial project set up

---
+++ db/migrate/20221124070543_create_authors.rb
@@ -0,0 +1,12 @@
+class CreateAuthors < ActiveRecord::Migration[7.0]
+  def change
+    create_table :authors do |t|
+      t.string :firstname
+      t.string :lastname
+      t.string :email
+      t.integer :phone
+
+      t.timestamps
+    end
+  end
+end
