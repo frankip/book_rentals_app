bfcdc6f9a504084b820e73055feee0405647bebc
Franck <gwadafrank@gmail.com>
2022-11-24 10:29:39 +0300

    implemt authors controler methods

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,28 @@
 class AuthorsController < ApplicationController
+
+    def index
+        author = Author.all
+        render json: author, status: :ok
+    end
+
+    def show
+        author = find_by(id: params[:id])
+        if author
+            render json: author, status: :ok
+        else
+            render json: {error: "sorry book not found"}, status: :not_found
+        end
+
+    end
+
+    def create
+        author = Author.create(author_params)
+        render json: author, status: :created
+    end
+
+    private
+
+    def author_params
+        params.permit(:firstname, :last_name, :email :phone)
+    end
 end
